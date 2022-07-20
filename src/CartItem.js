1cba3f91fbab7752adf8265aead483e0b59144f5
pintu <jm0046476@gmail.com>
2022-07-20 10:54:55 +0530

    add CartItem with styling ok

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,52 @@
 import React from 'react';
-class CartItem extends React.component{
+class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:333,
+            title:"phone",
+            Qty:1,
+            img:'ok'
+        }
+       // this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+    increaseQuantity =()=>{
+        console.log('this',this.state);
+    }
+
     render(){
+        const{price,title,qty}=this.state;
         return (
             <div className='cart-item'>
             <div className='left-block'> 
 
             <img style={styles.image}/>
+            </div>
+
             <div className='right-block'> 
 
-            <div style={{fontSize:25}}>phone </div>
-            <div style={{color:'#777'}}> rs 8964413</div>
-            <div style={{color:'#777'}}>qty:1 </div>
+            <div style={{fontSize:25}}>{title} </div>
+            <div style={{color:'#777'}}> Rs {price}</div>
+            <div style={{color:'#777'}}>Qty {qty}</div>
             <div className='cart-item-actions'> </div>
+            <div className='cart-item-actions'>
+                <img alt='increase' 
+                className='action-icons'
+                 src='https://img.icons8.com/ios-glyphs/50/000000/plus--v1.png'
+                 onClick={this.increaseQuantity}/>
+                <img alt='decrease' 
+                className='action-icons' 
+                src='https://img.icons8.com/ios-filled/50/000000/minus.png'
+                />
+                <img alt='delete' 
+                className='action-icons' 
+                src='https://img.icons8.com/ios-glyphs/50/000000/filled-trash.png'
+                />
+
             {/* Buttons */}
             </div>
-            </div> </div>
+            </div>
+             </div>
         );
     }
 }
